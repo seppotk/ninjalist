9097d66edcee2bc082d0b635f85ed3eb3017001e
seppotk <seppo.kallio2@metropolia.fi>
2022-10-30 18:16:50 +0200

    Images and metadata 	modified:   comps/Navbar.js 	modified:   pages/404.js 	modified:   pages/about.js 	modified:   pages/index.js     new file:   public/logo.png

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,16 +1,17 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/logo.png" alt="site logo" width={128} height={77} />
       </div>
-      <Link href="/" legacyBehavior><a>Home</a></Link>
-      <Link href="/about" legacyBehavior><a>About</a></Link>
-      <Link href="/ninjas/" legacyBehavior><a>Ninja Listing</a></Link>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/ninjas/">Ninja Listing</Link>
     </nav>
-);
+  );
 }
  
 export default Navbar;
