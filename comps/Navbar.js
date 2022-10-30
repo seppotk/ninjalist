37e57ba88e0e399360454f7f576358d320db0d0b
seppotk <seppo.kallio2@metropolia.fi>
2022-10-30 17:00:36 +0200

    Tutorial 6 	new file:   comps/Layout.js 	modified:   comps/Navbar.js 	modified:   pages/_app.js 	modified:   pages/index.js 	modified:   styles/Home.module.css 	new file:   styles/Jobs.module.css 	modified:   styles/globals.css

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
       <div className="logo">
         <h1>Ninja List</h1>
       </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/about"><a>About</a></Link>
-      <Link href="/ninjas/"><a>Ninja Listing</a></Link>
+      <Link href="/" legacyBehavior><a>Home</a></Link>
+      <Link href="/about" legacyBehavior><a>About</a></Link>
+      <Link href="/ninjas/" legacyBehavior><a>Ninja Listing</a></Link>
     </nav>
 );
 }
